f1ed035be3d78cd17d88a2ce53f821b04e889034
MateoZambrano <zambrano.gabriel@correounivalle.edu.co>
2025-03-26 18:02:49 -0500

    Update of the pageChange

--- src/app/pages/products/features/product-list/product-list.component.ts
+++ src/app/pages/products/features/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProductsSateService } from '../../data-access/products-state.service';
 import { ProductCardComponent } from '../../../../components/product-card/product-card.component';
 import { CartStateService } from '../../shared/data-access/cart-state.service';
@@ -7,7 +8,7 @@ import { Product } from '../../shared/interfaces/product.interface';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [CommonModule,ProductCardComponent],
   templateUrl: './product-list.component.html',
   providers: [ProductsSateService],
 })
@@ -15,17 +16,38 @@ export default class ProductsListComponent {
   productsState = inject(ProductsSateService);
   cartState = inject(CartStateService).state;
 
+  constructor() {
+    this.productsState.changePage$.next(1);
+  }
+
   previousPage() {
-    const page = this.productsState.state.page() - 1;
-    this.productsState.changePage$.next(page);
+    const page = this.productsState.state.page();
+    if (page > 1) {
+      this.productsState.changePage$.next(page - 1);
+    }
   }
   nextPage() {
     const page = this.productsState.state.page() + 1;
     this.productsState.changePage$.next(page);
   }
-  
-  pageChanged(page: String) {
-    //const page = this.productsState.state.page();
+
+  pageChanged(page: string) {
+    const pageNumber = parseInt(page, 10);
+
+    if (!isNaN(pageNumber)) {
+      this.productsState.changePage$.next(pageNumber); //change page cuando es un numero
+    } else {
+      switch (page) {
+        case 'previous':
+          this.previousPage();
+          break;
+        case 'next':
+          this.nextPage();
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   addToCart(product: Product) {
