0b6510deab8196c5088d71d302ed02d644cacdae
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-26 22:58:25 -0500

    Fixing config files and installing some dependences

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+//1) IMPORTAR LOS COMPONENTES (RUTA...nombre.component)
 import { Routes } from '@angular/router';
 // Ruta por defecto
 import { HomeComponent } from './pages/home/home.component';
@@ -18,24 +19,45 @@ import { ProductsDashboardComponent } from './pages/products/dashboard/dashboard
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 import { MissingComponent } from './pages/missing/missing.component';
 
+//2) CREAR UN ARREGLO CON LAS RUTAS DE LA APLICACIÓN
 // Arreglo con las rutas de la aplicación
 export const routes: Routes = [
   // Por defecto
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'home', redirectTo: '', component: HomeComponent, pathMatch: 'full' },
   // Paginas de la aplicacion
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'settings', component: SettingsComponent, pathMatch: 'full' },
   { path: 'products', component: ProductsListComponent, pathMatch: 'full' },
   { path: 'cart', component: CartListComponent, pathMatch: 'full' },
-  // Páginas protegidas
-  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
+  // Páginas protegidas en el dashboard
+  { path: 'user-dashboard', title: 'User Dashboard component', component: UserDashboardComponent,
+    children: [
+      {
+        path: 'products', // child route path
+        component: ProductsDashboardComponent, // child route component that the router renders
+        pathMatch: 'full', canActivate: [NormalGuard]
+      },
+      {
+        path: 'orders', // child route path
+        component: OrdersDashboardComponent, // child route component that the router renders
+        pathMatch: 'full', canActivate: [NormalGuard]
+      },
+      {
+        path: 'users', // child route path
+        component: UsersDashboardComponent, // child route component that the router renders
+        pathMatch: 'full', canActivate: [NormalGuard]
+      },
+      {
+        path: 'metrics',
+        component: MetricsComponent, // another child route component that the router renders
+      },
+    ],
+    pathMatch: 'full', canActivate: [NormalGuard] },
   { path: 'admin-dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard] },
   // Páginas del dashboard
-  { path: 'dashboard-orders', component: OrdersDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
-  { path: 'dashboard-products', component: ProductsDashboardComponent, pathMatch: 'full' },
-  //{ path: 'admin-dashboard-product', component: DashboardComponentProduct, pathMatch: 'full', canActivate: [AdminGuard] },
-  //{ path: 'admin-dashboard-user', component: DashboardComponentUser, pathMatch: 'full', canActivate: [AdminGuard] },
+  //{ path: "user/:userId", component: OtherComponent },
   { path: 'unauthorized', component: UnauthorizedComponent }, // ruta no autorizada
   { path: '**', component: MissingComponent }, // ruta no encontrada
 ];
