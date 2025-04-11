f67f82761d0b40f63a3551635210113d62dc0ba0
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-10 19:01:41 -0500

    Feat: Creating a new service to prepare SaaS
    Configuring a service to retrieve css configuration  for on-site clients

---
+++ src/app/services/dynamic-theme.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DynamicThemeService {
+  // Simula un fetch a la base de datos
+  getThemeColors(): Observable<any> {
+    const theme = {
+      navbar: {
+        background: '#1E3A8A',
+        text: '#FFFFFF',
+      },
+      button: {
+        fondo: '#1E3A8A',
+        texto: '#FFFFFF',
+        fondoHover: '#FFFFFF40',
+        textoHover: '#000000'
+      },
+      sidebar: {
+        background: '#111827',
+        text: '#FFFFFF'
+      },
+      title: {
+        color: '#1E40AF'
+      },
+      background: '#F3F4F6'
+    };
+    // más adelante será un fetch a tu API
+    return of(theme); // Devuelve un observable como si fuera de una API
+  }
+}
