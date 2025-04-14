158a63201d9660b7efa722ba36448d8e28661bfb
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-13 23:48:12 -0500

    Feat: Applying all dynamic complement in sidebar
    Standard colors and effects in sidebar and creating inventory component to admin dashboard

---
+++ src/app/pages/admin/admin-dashboard/inventory-dashboard/inventory-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-dashboard',
+  imports: [],
+  templateUrl: './inventory-dashboard.component.html',
+  styleUrl: './inventory-dashboard.component.css'
+})
+export class InventoryDashboardComponent {
+
+}
