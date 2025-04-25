6dedc0e127838ead3052a0f3082b5cd0bd36ead8
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-25 13:25:45 -0500

    Feat: Building base structure for contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm = {
+    name: '',
+    email: '',
+    message: ''
+  };
 
+  onSubmit() {
+    console.log('Formulario enviado:', this.contactForm);
+    // Aquí podrías agregar lógica para enviar a backend, etc.
+  }
 }
