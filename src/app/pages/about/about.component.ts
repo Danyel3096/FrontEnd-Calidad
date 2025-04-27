dccba484140d29e8f6aae31c29c687d28c2dd96e
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-26 23:53:07 -0500

    Feat: Improving help component
    Adding content more real with accordions

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DynamicCardComponent } from '../../components/dynamic-card/dynamic-card.component';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule],
+  imports: [CommonModule, DynamicCardComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-help',
-  imports: [],
+  imports: [NgbAccordionModule],
   templateUrl: './help.component.html',
   styleUrl: './help.component.css'
 })
 export class HelpComponent {
-
+  items = ['First', 'Second', 'Third'];
 }
