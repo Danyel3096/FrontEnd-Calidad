e6a895baf68a72dd06d5e2fe0487aa751809785c
MateoZambrano <zambrano.gabriel@correounivalle.edu.co>
2025-05-18 18:19:42 -0500

    Efect of images and translator

--- src/app/pages/products/product-detail/product-detail.component.ts
+++ src/app/pages/products/product-detail/product-detail.component.ts
@@ -1,13 +1,14 @@
 import { Component, effect, inject, input } from '@angular/core';
 import { ProductDetailStateService } from '../../../services/product-detail-state.service';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, CommonModule } from '@angular/common';
 import { CartStateService } from '../../../services/cart-state.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CurrencyPipe],
-  templateUrl: './product-detail.component.html',
+  imports: [ CommonModule, CurrencyPipe],
+  templateUrl: './product-detail.component.html', 
+  styleUrls: ['./product-detail.component.css'],
   providers: [ProductDetailStateService],
 })
 export default class ProductDetailComponent {
@@ -15,6 +16,12 @@ export default class ProductDetailComponent {
   cartState = inject(CartStateService).state;
 
   id = input.required<string>();
+
+  // 360 View state
+  is360Mode = false;
+  totalFrames = 36;
+  currentFrame = 1;
+  currentFrameUrl = this.getFrameUrl(1);
 
   constructor() {
     effect(() => {
@@ -27,5 +34,26 @@ export default class ProductDetailComponent {
       product: this.productDetailState.product()!,
       quantity: 1,
     });
+  }
+
+  toggle360() {
+    this.is360Mode = !this.is360Mode;
+    this.currentFrame = 1;
+    this.currentFrameUrl = this.getFrameUrl(1);
+  }
+
+  onMouseMove(event: MouseEvent) {
+    const containerWidth = (event.target as HTMLElement).clientWidth;
+    const mouseX = event.offsetX;
+    const percent = mouseX / containerWidth;
+    const frame = Math.ceil(percent * this.totalFrames);
+
+    this.currentFrame = frame;
+    this.currentFrameUrl = this.getFrameUrl(frame);
+  }
+
+  getFrameUrl(frame: number): string {
+    const padded = frame.toString().padStart(3, '0');
+    return `assets/images/shirt360/shirt_${padded}.png`;
   }
 }
