0b3d836d2d29ee14b237e34510c0dbb45289f84f
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-18 19:21:38 -0500

    Feat: Applying dynamic colors in product list

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,17 +1,43 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '../../interfaces/product.interface';
 import { RouterLink } from '@angular/router';
+
+import { DynamicThemeService } from '../../services/dynamic-theme.service';
+import { ThemeColors } from '../../interfaces/dynamic-colors.interface';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-card.component.html',
   styles: ``,
 })
-export class ProductCardComponent {
+
+export class ProductCardComponent implements OnInit {
   @Input() product!: Product; // Recibe el producto como input
   @Output() addToCart = new EventEmitter<Product>(); // Emite el producto al agregar al carrito
+
+  activePalette!: ThemeColors;
+
+  constructor(private dynamicThemeService: DynamicThemeService, /* … */) { }
+
+  homePageColor: ThemeColors['homePage'] = {
+    backgroundPrimary: '',
+    backgroundSecondary: '',
+    backgroundTertiary: '',
+    backgroundQuaternary: '',
+    textTitle: '',
+    textBody: ''
+  };
+
+  ngOnInit(): void {
+    // SUSCRÍBETE a la sección 'home page' del tema activo
+    this.dynamicThemeService.getSection('homePage').subscribe(colors => {
+      this.homePageColor = colors;
+      console.log('Footer colors:', this.homePageColor);
+    });
+  }
 
   add(event: Event) {
     event.stopPropagation();
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -4,7 +4,7 @@ import { DynamicCardComponent } from '../../components/dynamic-card/dynamic-card
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, DynamicCardComponent],
+  imports: [CommonModule], //DynamicCardComponent ERROR PENDIENTE
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
