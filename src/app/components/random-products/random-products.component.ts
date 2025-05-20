29c2429033d77f606adfade8c05e0b04f400be8f
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-20 14:23:34 -0500

    Feat: Creating a new component random products for product detail

---
+++ src/app/components/random-products/random-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-products',
+  imports: [],
+  templateUrl: './random-products.component.html',
+  styleUrl: './random-products.component.css'
+})
+export class RandomProductsComponent {
+
+}
