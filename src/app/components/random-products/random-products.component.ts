c85318c6b23fef97512e2410ee4a3fd7dab7c4fe
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-20 16:57:50 -0500

    Fix: Issue when click in a similar product by random suggestion
    After changing to the selected product successfully the similar products doesnt refresh it, but now it does

--- src/app/components/random-products/random-products.component.ts
+++ src/app/components/random-products/random-products.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule, SlicePipe } from '@angular/common';
 import { ProductsService } from '../..//services/product.service';
 import { Product } from '../../interfaces/product.interface';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -14,9 +14,18 @@ import { RouterLink } from '@angular/router';
 export class RandomProductsComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductsService) {}
+  constructor(
+    private productService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(() => {
+      this.loadRandomProducts();
+    });
+  }
+
+  loadRandomProducts() {
     this.productService.getRandomProducts(3).subscribe({
       next: (data: Product[]) => this.products = data,
       error: (err: any) => console.error('Error cargando productos aleatorios', err)
