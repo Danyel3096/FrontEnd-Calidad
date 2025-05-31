2728e135084fef254a0215ec51805f49ab84cb57
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-31 00:43:05 -0500

    Feat: Improving style of cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,34 +1,43 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CartItemComponent } from '../../components/cart-item/cart-item.component';
 import { CartStateService } from '../../services/cart-state.service';
 import { ProductItemCart } from '../../interfaces/product.interface';
 import { CurrencyPipe } from '@angular/common';
+import { Product } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartItemComponent, CurrencyPipe],
+  imports: [CommonModule, CartItemComponent],
   templateUrl: './cart.component.html',
   styles: ``,
 })
+
 export default class CartComponent {
   state = inject(CartStateService).state;
 
+  isLoading = false;
+  
   onRemove(id: number) {
     this.state.remove(id);
   }
 
-  onIncrease(product: ProductItemCart) {
+  onIncrease(productItemCart: ProductItemCart) {
     this.state.udpate({
-      product: product.product,
-      quantity: product.quantity + 1,
+      product: productItemCart.product,
+      quantity: productItemCart.quantity + 1,
     });
   }
 
-  onDecrease(product: ProductItemCart) {
+  onDecrease(productItemCart: ProductItemCart) {
     this.state.udpate({
-      ...product,
-      quantity: product.quantity - 1,
+      ...productItemCart,
+      quantity: productItemCart.quantity - 1,
     });
+  }
+
+  trackById(index: number, productItemCart: ProductItemCart): number {
+    return productItemCart.product.id;
   }
 }
