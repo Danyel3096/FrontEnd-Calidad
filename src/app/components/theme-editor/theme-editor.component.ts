0a86114142899c352881eab760c12165afc19e51
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-06-01 21:59:04 -0500

    Feat: Creating component for dynamic edition of website colors

---
+++ src/app/components/theme-editor/theme-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-editor',
+  imports: [],
+  templateUrl: './theme-editor.component.html',
+  styleUrl: './theme-editor.component.css'
+})
+export class ThemeEditorComponent {
+
+}
