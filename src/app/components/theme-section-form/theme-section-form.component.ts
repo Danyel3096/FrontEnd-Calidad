71ed135b326f0d806530224310761cd04d2d5b97
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-06-04 23:51:45 -0500

    Feat: Creating new component for customization module

---
+++ src/app/components/theme-section-form/theme-section-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-section-form',
+  imports: [],
+  templateUrl: './theme-section-form.component.html',
+  styleUrl: './theme-section-form.component.css'
+})
+export class ThemeSectionFormComponent {
+
+}
